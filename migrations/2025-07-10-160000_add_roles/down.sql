3197ab43aee8a4b7a635fde37feff0e5f121aa32
James Vasile <james@jamesvasile.com>
2025-07-10 15:14:57 -0400

    Fix diesel.toml, add role migration

---
+++ migrations/2025-07-10-160000_add_roles/down.sql
@@ -0,0 +1,5 @@
+-- Remove the four specific roles from the roles table
+
+DELETE FROM roles
+WHERE name IN ('newtown-admin', 'newtown-staff', 'admin', 'user');
+
---
+++ migrations/2025-07-10-160000_add_roles/up.sql
@@ -0,0 +1,8 @@
+-- Insert initial roles into the roles table
+
+INSERT INTO roles (name, description) VALUES
+  ('newtown-admin', 'Administrator for Newtown'),
+  ('newtown-staff', 'Staff member for Newtown'),
+  ('admin', 'Administrator for Site Owner'),
+  ('user', 'User');
+
