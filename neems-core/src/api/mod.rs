ab1bd149b351e31c5655b233ace950cd04b99c8f
James Vasile <james@jamesvasile.com>
2025-07-17 05:47:30 -0400

    Split general into two files

--- neems-core/src/api/mod.rs
+++ neems-core/src/api/mod.rs
@@ -1,15 +1,17 @@
-pub mod general;
+pub mod fixphrase;
 pub mod institution;
 pub mod role;
+pub mod status;
 pub mod user;
 
 use rocket::Route;
 
 pub fn routes() -> Vec<Route> {
     let mut routes = Vec::new();
-    routes.extend(general::routes());
+    routes.extend(fixphrase::routes());
     routes.extend(institution::routes());
     routes.extend(role::routes());
+    routes.extend(status::routes());
     routes.extend(user::routes());
     routes
 }
